873b9aade93e7dc292df4418f5113ecdb1d09e37
tv-vivo <botandresvpn@gmail.com>
2024-08-07 18:39:25 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+const canciones = [
+        {
+            titulo: "Canción 1",
+            img: "https://telegra.ph/file/7af07da609c73477db23f.jpg",
+            autor: "Autor 1",
+            url: "https://streamingdevz.netlify.app/1.mp3"
+        },
+        {
+            titulo: "Canción 2",
+            img: "https://telegra.ph/file/8ed720d893defc17cc535.jpg",
+            autor: "Autor 2",
+            url: "https://streamingdevz.netlify.app/5.mp3"
+        },
+        {
+            titulo: "Canción 3",
+            img: "https://telegra.ph/file/8ed720d893defc17cc535.jpg",
+            autor: "Autor 3",
+            url: "https://streamingdevz.netlify.app/13.mp3"
+        }
+        // Agrega más canciones aquí si es necesario
+    ];
