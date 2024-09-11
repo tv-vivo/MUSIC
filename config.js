a85b1d80fa4c9eea34be6c3dad23043f24494505
tv-vivo <botandresvpn@gmail.com>
2024-09-10 20:17:08 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,9 @@
 const canciones = [
         {
-            titulo: "LOS NO TAN TRISTE",
-            img: "https://telegra.ph/file/a7b1ba059edef6577d062.jpg",
-            autor: "Nanpa Básico / Gera MX / Charles Ans",
-            url: "http://tv-vivo.github.io/MUSIC/MUSIC/los%20no%20tan%20tristes.mp3"
-        },
-        {
-            titulo: "BESAME SIN SENTIR",
-            img: "https://telegra.ph/file/8adf71f6ed765d531d75c.jpg",
-            autor: "Micro TDH",
-            url: "https://tv-vivo.github.io/MUSIC/MUSIC/%F0%9D%99%B1%F0%9D%9A%8E%F0%9D%9A%9C%F0%9D%9A%8A%F0%9D%9A%96%F0%9D%9A%8E%20%F0%9D%9A%82%F0%9D%9A%92%F0%9D%9A%97%20%F0%9D%9A%82%F0%9D%9A%8E%F0%9D%9A%97%F0%9D%9A%9D%F0%9D%9A%92%F0%9D%9A%9B.mp3"
+            titulo: "AURA",
+            img: "https://tv-vivo.github.io/MUSIC/img/AURA.jpg",
+            autor: "Aura",
+            url: "http://tv-vivo.github.io/MUSIC/download/AURA.m4a"
         },
         // Agrega más canciones aquí si es necesario
     ];
