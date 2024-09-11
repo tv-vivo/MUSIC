7e2b2f7992a4adb09a8d7ece9d41a440a08e36e2
tv-vivo <botandresvpn@gmail.com>
2024-09-10 20:24:42 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,14 @@ const canciones = [
         {
             titulo: "AURA",
             img: "https://tv-vivo.github.io/MUSIC/img/AURA.jpg",
-            autor: "Aura",
+            autor: "V-Aura",
             url: "http://tv-vivo.github.io/MUSIC/download/AURA.m4a"
+        },
+        {
+            titulo: "ELECTRO RITMO",
+            img: "https://tv-vivo.github.io/MUSIC/img/electroritmo.jpg",
+            autor: "V-Aura",
+            url: "https://tv-vivo.github.io/MUSIC/download/electroritmo.aac"
         },
         // Agrega más canciones aquí si es necesario
     ];
